391f8d2691adda39b0c3754d0202342fff69d363
avvorstenbosch <alex_van_vorstenbosch@hotmail.com>
2022-11-23 00:55:05 +0100

    Check if audio files are complete, download incomplete ones again

---
+++ speech_to_text/transcribe_incomplete.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is used to process al the audio-files in the '../data/audio' directory
+# into text files in the '../data/text' directory 
+current_dir=$(pwd)
+cd ../data/audio
+while read mp3;
+    do whisper "file:${mp3}" --model large --language Dutch --output_dir ../data/text/;
+done <../../extract_data/incomplete_mp3.txt
+cd $current_dir
