e755632de6b8932540fc56087781ee449587d664
avvorstenbosch <alex_van_vorstenbosch@hotmail.com>
2022-12-07 23:05:29 +0100

    Finished quality check code on mp3 files

--- speech_to_text/transcribe_incomplete.sh
+++ speech_to_text/transcribe_incomplete.sh
@@ -5,6 +5,6 @@
 current_dir=$(pwd)
 cd ../data/audio
 while read mp3;
-    do whisper "file:${mp3}" --model large --language Dutch --output_dir ../data/text/;
+    do whisper "file:${mp3}" --model large --language Dutch --output_dir ../text/;
 done <../../extract_data/incomplete_mp3.txt
 cd $current_dir
